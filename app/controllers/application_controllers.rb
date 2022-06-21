846984a2ea3b7f59d82990d7a2beecfd9ecc8c8a
Mark <markwilkins86@gmail.com>
2022-06-21 16:05:55 -0400

    Mark Push

--- app/controllers/application_controllers.rb
+++ app/controllers/application_controllers.rb
@@ -2,8 +2,15 @@ class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
   # Add your routes here
-  get "/" do
-    { message: "Good luck with your project!" }.to_json
+  get "/coffees" do
+    coffee = Coffee.all
+    coffee.to_json
+  end
+
+  delete '/coffees/:id' do
+    coffee = Coffee.find(params[:id])
+    coffee.destroy
+    coffee.to_json
   end
 
 end
